59013e20a5e3aea46189d336286a8632b68d7fa0
Igor Pinheiro Ribeiro <igor.ribeiro@globalsys.com.br>
2025-07-31 22:44:00 -0300

    feat: refactor home; create extensions class; config function

---
+++ finup/Model/Transaction.swift
@@ -0,0 +1,17 @@
+//
+//  Transaction.swift
+//  finup
+//
+//  Created by Igor Pinheiro Ribeiro on 31/07/25.
+//
+
+import Foundation
+
+
+struct TransactionData: Codable {
+    let icon: String
+    let name: String
+    let date: String
+    let value: String
+    let subIcon: String
+}
