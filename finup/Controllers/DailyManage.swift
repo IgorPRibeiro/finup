647fec5b9427e5719b8941d85081caea68e0e9db
Igor Ribeiro <igor.ribeiro@globalsys.com.br>
2026-06-15 23:21:58 -0300

    store

---
+++ finup/Controllers/DailyManage.swift
@@ -0,0 +1,14 @@
+//
+//  DailyManage.swift
+//  finup
+//
+//  Created by Igor Pinheiro Ribeiro on 30/03/26.
+//
+
+import UIKit
+
+class DailyManageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
